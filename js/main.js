1981a6ff9ab052156514e1873ac7081084e0ab3b
nelkit <nelkitisael792@gmail.com>
2014-12-06 14:55:17 -0600

    Terminada Inicio Movil

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+	$('.wrapper').blurjs({
+		overlay: 'rgba(0,0,0,0.4)',
+		radius:50
+	});
+
+	$( document ).on( 'click', '#menu', function () {
+		var me = $(this);
+		var menuPanel = $('.menuBtn');
+		var fotoPerfil = $('.fotoPerfil');
+		var estado = me.attr('data-state');
+
+		if (estado==="plegado") {
+			menuPanel.animate({
+				height: '300px'
+			}, 600).css({background:'rgba(0,0,0,.4)'});
+			me.attr('data-state','desplegado');
+			fotoPerfil.css({
+				margin: '12px auto 38px auto' 
+			});
+		}else if (estado==="desplegado") {
+			menuPanel.animate({
+				height: '72px'
+			}, 600).css({background:'none'});
+			me.attr('data-state','plegado');
+			fotoPerfil.css({
+				margin: '-30px auto 38px auto' 
+			});
+		};
+	});
+});
