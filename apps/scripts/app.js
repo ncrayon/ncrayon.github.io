8b46110931f1fc97c29725f0173d6e66d2a9a716
nelkit <nelkitisael792@gmail.com>
2014-12-15 16:54:47 -0600

    modificado comentarios al blog

--- apps/scripts/app.js
+++ apps/scripts/app.js
@@ -11,28 +11,24 @@ angular.module('BlogApp',[
       .when('/blog', {
         templateUrl: 'views/blog.html',
         controller: 'BlogCtrl',
-        title: 'Blog de UI/UX, Front End & Back End Developer',
         isActiveBlog: 'blogActive',
         isActiveProject: ''
     })
       .when('/blog/:id', {
         templateUrl: 'views/details.blog.html',
         controller: 'DetailsBlogCtrl',
-        title: 'Blog de UI/UX, Front End & Back End Developer',
         isActiveBlog: 'blogActive',
         isActiveProject: ''
     })
       .when('/projects/', {
         templateUrl: 'views/projects.html',
         controller: 'ProjectsCtrl',
-        title: 'Mis Proyectos',
         isActiveBlog: '',
         isActiveProject: 'projectActive'
     })
       .when('/projects/:id', {
         templateUrl: 'views/details.projects.html',
         controller: 'DetailsProjectsCtrl',
-        title: 'Mis Proyectos',
         isActiveBlog: '',
         isActiveProject: 'projectActive'
     })
@@ -42,7 +38,6 @@ angular.module('BlogApp',[
  })
 .run(['$location', '$rootScope', function($location, $rootScope) {
     $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
-        $rootScope.title = current.$$route.title;
         $rootScope.isActiveBlog = current.$$route.isActiveBlog; 
         $rootScope.isActiveProject = current.$$route.isActiveProject;
     });
